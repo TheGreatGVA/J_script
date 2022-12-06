cc58d587dd0e4770a5293e59ae762ab2948a1ac0
Aditya <adityagv16@gmail.com>
2022-12-06 10:10:31 +0000

    Create 5 files

---
+++ hello.js
@@ -0,0 +1,56 @@
+var a;//can also be changed after assigning
+let b;//can be changed after assiging
+const c;// cannot be changed after assiging and is the best type of variable declartion
+
+console.log(
+    a,
+    b,
+    c
+)
+
+
+//var a=12131
+//a=234
+//let b;
+//b=225144
+
+//const c=584433
+// IF ELSE BLOCK
+/*
+if (a>b){ 
+    console.log("a is greater than b")
+} else if (a<b){ 
+    console.log("a is less than b")
+}*/
+
+//FOR BLOCK
+/*
+for(let i=0;i<10;i++){ 
+    console.log(i)
+}*/
+
+//json - java script object notation
+/*
+const a = { 
+    name: 'Aditya',
+    surname: 'GV',
+    school: {
+        name: "fiitjee",
+    }
+};
+console.log(a.name)
+console.log(a.school.name)
+const arr=[1,2,3,4]
+console.log(arr[2])
+
+try {
+    console.log(a.univ.name)
+} catch (err){
+    console.log("somethings wrong")
+}*/
+
+function square(n) {
+    return n*n
+}
+
+console.log(square(12))
